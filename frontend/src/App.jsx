87cb275bb0dc7c2c38f1f8898ab9b8d1ac186320
Aamir <aamir576shaikhl@gmail.com>
2026-04-15 16:29:41 +0530

    feat: implement Google OAuth authentication with Login and Success routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
+import Success from './components/Success'
 import './App.css'
 
 function App() {
- 
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/success" element={<Success />} />
+    </Routes>
   )
 }
 
