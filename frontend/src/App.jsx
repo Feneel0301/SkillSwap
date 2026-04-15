bc4f466a4454a379274d30b58655666d02cb651a
Aamir <aamir576shaikhl@gmail.com>
2026-04-15 17:50:08 +0530

    refactor: remove Google OAuth authentication, routing, and unused dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,8 @@
-import { Routes, Route } from 'react-router-dom'
-import Login from './components/Login'
-import Success from './components/Success'
-import './App.css'
+import React from 'react'
 
-function App() {
+export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/success" element={<Success />} />
-    </Routes>
+    <>
+    </>
   )
 }
-
-export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { GoogleOAuthProvider } from '@react-oauth/google'
-import { BrowserRouter } from 'react-router-dom'
+
 import './index.css'
 import App from './App.jsx'
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </GoogleOAuthProvider>
+    <App />
+
   </StrictMode>,
 )
